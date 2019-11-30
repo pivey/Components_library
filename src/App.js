bfb34b3a3b8f99f9e1107962169157bff177495d
peter ivey-hansen <peteriveyhansen@gmail.com>
2019-11-30 14:21:46 +0100

    adding pop up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
+import './styles/base.css';
+import LoginUseReducer from './LoginUseReducer';
+import LoginUseReducerImmer from './LoginUseReducerImmer';
+
+
+function useLocationHash() {
+  const [hash, setHash] = useState(window.location.hash);
+  function onHashChange() {
+    setHash(window.location.hash);
+  }
+  useEffect(() => {
+    window.addEventListener('hashchange', onHashChange);
+    return () => window.removeEventListener('hashchange', onHashChange);
+  }, []);
+  return hash;
+}
+
+function useSimpleHashRouter(routes) {
+  const hash = useLocationHash();
+  // Exclude '#' when calculating hash.
+  const currentRoute = routes[hash.substr(1)];
+  if (currentRoute) {
+    return currentRoute;
+  }
+  return null;
+}
 
 function App() {
+  const CurrentRoute = useSimpleHashRouter({
+    useReducer: LoginUseReducer,
+    useReducerImmer: LoginUseReducerImmer,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!CurrentRoute && (
+        <div className="App App-Column">
+          <a href="#useReducer">useReducer</a>
+          <br />
+          <br />
+          <a href="#useReducerImmer">useReducerImmer</a>
+        </div>
+      )}
+      {CurrentRoute && <CurrentRoute />}
+    </>
   );
 }
 
