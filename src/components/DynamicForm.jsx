696a6385f5e10a7abae23e720b05720fccdeb11d
peter ivey-hansen <peteriveyhansen@gmail.com>
2020-01-09 22:49:07 +0100

    added popup button

--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -1,12 +1,8 @@
 import React, { useReducer, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { flex, noSelect } from '../utils';
 import FocusEffectInput from './FocusEffectInput';
 import penicon from '../icons/pen-square-solid.svg'
-
-const sayHi = (input) =>  {
-    console.log(`${input}`)
-}
 
 const PageWrapper = styled.div`
     ${flex('flex-start', 'flex-start')}
@@ -21,7 +17,6 @@ const BackgroundText = styled.p`
     font-size:1.8rem;
     text-align:justify;
     line-height:1.4;
-    z-index:998;
 `;
 
 const Modal = styled.div`
@@ -31,9 +26,11 @@ const Modal = styled.div`
     background:rgba(38, 12, 12, 0.15);
     z-index:999;
     position:fixed;
+    visibility: ${({visible}) => visible ? 'visible' : 'hidden'};
 `;
 
 const FormMother = styled.div`
+    transition: all
     height:auto;
     width:auto;
     background:white;
@@ -60,7 +57,7 @@ const FormSubmitBtn = styled.button`
     ${noSelect}
     font-weight:bold;
     border-radius:5px;
-    transition: all 0.2s
+    transition: all 0.2s;
     padding: 1rem 2.5rem;
     font-size:1.4rem
     margin:2.5rem 0rem 1rem 0rem;
@@ -150,7 +147,8 @@ const IconHolder = styled.div`
 `;
 
 const PenDiv = styled.img`
-    transition: all 1.2s ease-in-out;
+    transition: 0s;
+    // transition: all 1.2s ease-in-out;
     height:8rem;
     width:8rem;
     filter: invert(13%) sepia(99%) saturate(6389%) hue-rotate(266deg) brightness(91%) contrast(110%);
@@ -163,7 +161,66 @@ const PenDiv = styled.img`
     transform: rotate(360deg);
     filter: invert(38%) sepia(75%) saturate(1338%) hue-rotate(154deg) brightness(100%) contrast(88%);
     }
-    
+`;
+
+const slideInFromLeft = keyframes`
+  0% {
+    transform: translateX(-200%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+`
+
+const slideBackLeft = keyframes`
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-200%);
+  }
+`
+
+const FormOpener = styled.button`
+    transition: all 0.2s;
+    // animation-duration: 1s; /* the duration of the animation */
+    // animation-timing-function: ease-out; /* how the animation will behave */
+    // animation-delay: 0s; /* how long to delay the animation from starting */
+    // animation-iteration-count: 1; /* how many times the animation will play */
+    // animation-name: slideInFromLeft; /* the name of the animation we defined above */
+    animation: 0.7s cubic-bezier(.57,.21,.69,1.25) 0s 1 ${({ animate }) => animate ? slideBackLeft : slideInFromLeft};
+    animation-fill-mode: forwards;
+    width:auto;
+    height:auto; 
+    font-size:2rem;
+    font-weight:bold;
+    position:absolute;
+    top:10rem;
+    left:5rem
+    padding:1.2rem;
+    border: 2px solid transparent;
+    border-radius:5px;
+    background: black;
+    color:white;
+    &:hover {
+        cursor: pointer;
+        border: 2px solid white;
+        background: #6610F2;
+        color:white;
+        transform: scale(0.95)
+    }
+`;
+
+const FormCloser = styled.div`
+    width: 20px;
+    height:20px;
+    background: green; 
+    position:absolute;
+    top:0px;
+    left:0px;
+    &:hover {
+        cursor:pointer;
+    }
 `;
 
 function dynamicFormReducer(state, action) {
@@ -200,6 +257,7 @@ const DynamicForm = () => {
     const [state, dispatch] = useReducer(dynamicFormReducer, initState);
     const [currentRef, setCurrentRef] = useState(null);
     const [fillInputs, setfillInputs] = useState(false);
+    const [openForm, setOpenForm] = useState(false);
 
     const inputFields = {
         name: /^[a-z\d]{1,15}$/i,
@@ -305,7 +363,6 @@ const DynamicForm = () => {
         focusedLabelSize: '1.3rem',
         inputFontSize: '1.5rem',
         initFontColor: '#454745',
-        onClick: sayHi,
         onChange: dispatch,
     }
 
@@ -321,18 +378,20 @@ const DynamicForm = () => {
     return (
         <>
             <PageWrapper>
+                <FormOpener type="button" animate={openForm} onClick={() => setOpenForm(!openForm)}>OpenForm</FormOpener>
                 <BackgroundText>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis nisi quam quos quae, quaerat corporis nemo aut mollitia voluptas dolorum ullam quo, eum eligendi alias debitis illum dolores placeat atque culpa repellendus ea accusamus consequatur ipsam velit! Possimus labore velit consectetur? Ratione praesentium accusantium cum laudantium, nemo explicabo nulla quasi deleniti, ut modi, perspiciatis voluptas eaque at quia? Asperiores, obcaecati repellat consequatur, a laudantium officia corrupti, culpa tempora sapiente assumenda praesentium voluptatum dolorem sint debitis exercitationem sunt dolor nostrum enim. Dicta voluptatibus repellat facere, ex cumque eligendi dolorum facilis nobis fugit voluptates expedita consequuntur quod assumenda tempora dolores fuga rerum officia autem explicabo. Beatae dolorem eius incidunt omnis, nulla eveniet assumenda quae odit ad cumque voluptates perferendis ab, vitae ipsum consectetur provident vel fuga neque id minus pariatur deleniti. Tempora dolorum ratione ipsa hic expedita! Porro distinctio asperiores vel? Beatae adipisci numquam veritatis in sequi, laudantium porro similique illum sed iste fuga sint totam. Dicta, minus rerum quae culpa eos neque tempore veritatis, placeat aut, enim quos sint ut iusto perferendis. Nobis corporis quae doloremque eius qui distinctio repellat ipsa quas amet porro aliquid omnis fuga repudiandae magni debitis laudantium sapiente molestiae deleniti, quos in accusantium fugit. Sit officiis, tenetur optio blanditiis maxime ex commodi deleniti sint, nam atque minus rerum placeat ut natus, quia unde excepturi aperiam accusantium libero! Accusantium vero voluptatum similique porro, aspernatur asperiores? Consectetur asperiores quia perspiciatis voluptate nesciunt ex tenetur pariatur aperiam veritatis, distinctio enim omnis natus inventore sapiente. Id vitae consectetur accusamus dolor reprehenderit quaerat voluptatem, error vel enim quae ab. Delectus tempore labore neque dolore voluptas maxime est laborum. Illo maiores sapiente quibusdam dolorum. Aut ad doloremque fugit veritatis nostrum molestias molestiae rem a vel dolor, consequatur at corrupti unde in numquam error, nisi harum impedit atque ullam iure dolores sit optio debitis? Obcaecati soluta recusandae veritatis, quae sequi libero sit. Quaerat quia architecto voluptates illum ullam laboriosam maxime, ipsum, doloribus accusantium dolores quibusdam voluptas voluptatibus repellat. Fugit illum nemo incidunt eaque dolorum, iste libero placeat rem autem cupiditate ea voluptas delectus, corrupti dolorem error dolor nihil maiores sunt eveniet! Dolores esse vitae voluptates, rem quibusdam inventore corporis distinctio nulla odit alias, id quasi earum quos doloribus ullam repudiandae fugit aspernatur sequi necessitatibus consequatur magnam nostrum blanditiis illo. Voluptates voluptas consequuntur assumenda laboriosam. Commodi dolores minus dicta excepturi provident similique, at sint ipsam corrupti adipisci, consequuntur asperiores ducimus esse odit, odio eveniet laboriosam.</BackgroundText>
                 <BackgroundText>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur eos exercitationem aliquam. Debitis est similique veniam nobis ea eius impedit, ex consequatur eum assumenda odit et, ut quae facilis accusamus! Saepe maxime dignissimos sunt necessitatibus corporis. Qui facilis pariatur sequi quidem aperiam! Perspiciatis similique officiis dolore dolorem consequuntur unde eligendi, illum excepturi omnis quibusdam quas id repudiandae aperiam dolores architecto consectetur libero? Ipsum fuga numquam atque quod aliquid culpa esse obcaecati ab enim, suscipit, dolorem nostrum magnam vel laboriosam eos nemo similique voluptas, iste animi? Exercitationem vel rerum cupiditate numquam tempora, quaerat doloremque non culpa explicabo consequuntur nam amet tempore eos doloribus quisquam molestiae ipsum placeat sunt. Eligendi cupiditate modi ducimus dignissimos fugit. Illum, similique. Illum asperiores quisquam fugit excepturi doloremque, laborum aspernatur reprehenderit et neque! Placeat hic sapiente deserunt suscipit molestias porro, accusantium consequatur, eum quo facere delectus dolor aliquam accusamus aut sit illo! Iure nostrum repellendus assumenda possimus? Et ex soluta nisi rem ea consequuntur voluptate vitae fugiat veniam error asperiores doloremque, hic voluptates incidunt, repudiandae qui veritatis nam vero? Ea deserunt fuga, eos modi recusandae velit quia ducimus dolor, dolorem iste quaerat, ullam nam. Necessitatibus doloremque fuga, repudiandae vero quas aspernatur saepe atque doloribus accusantium placeat optio rerum. Aperiam totam labore aspernatur! Hic facilis quibusdam modi. Veniam ducimus voluptatum eum! Vel repellat facere, voluptatem totam nobis quo ratione. Dolores dolorem fugiat aspernatur illum quos! Hic reprehenderit quae beatae, inventore rerum accusamus odit nam porro debitis fuga cum ratione consectetur totam. Deleniti, in. Magnam qui commodi asperiores quidem aliquid praesentium eum necessitatibus a accusamus itaque illum placeat sit nam fugit neque temporibus quae deserunt laboriosam, corporis facere architecto quia? Eos debitis quaerat officia suscipit voluptates libero, repellendus amet earum quasi aliquid ipsa nihil laborum expedita, dolorem minima, est neque consequatur obcaecati quam provident. Consectetur, enim? Dolor, quas ab corrupti aut magni porro cum corporis nemo optio qui odit. Nihil mollitia tenetur saepe doloremque voluptates hic dolor facilis labore, delectus nulla non eum dicta id consequatur obcaecati veritatis praesentium animi aspernatur, voluptatum, modi autem sapiente illum sunt. Quam aspernatur nostrum quis hic et numquam explicabo tempora, incidunt optio quidem ex corporis sit, consectetur impedit repellendus delectus voluptatum nemo magni voluptatibus excepturi error unde reiciendis? Reprehenderit, dolore in libero quibusdam, optio pariatur velit repellat ullam nisi iusto magnam exercitationem beatae illo praesentium, soluta nostrum asperiores! Incidunt repellendus maiores eos deserunt unde laborum sit est eveniet sed officiis, a minima modi.</BackgroundText>
             </PageWrapper>
-        <Modal>
+        <Modal visible={openForm}>
             <ButtonHolder>
                 <FillInputBtn onClick={() => insertCorrectInfo()}>Correct</FillInputBtn>
                 <FillInputBtn onClick={() => insertIncorrectInfo()}>Incorrect</FillInputBtn>
             </ButtonHolder>
             <FormMother>
+                    <FormCloser onClick={() => setOpenForm(!openForm)}/>
                     <FormHolder autoComplete="new-password">
                         <Banner>Information</Banner>
-                        <IconHolder><PenDiv src={penicon}></PenDiv></IconHolder>
+                        <IconHolder><PenDiv src={penicon} trans={openForm}></PenDiv></IconHolder>
                     <LabelInputHolder>
                         <FocusEffectInput 
                             stateName='name'
